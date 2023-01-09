58dbc86238ed84f09a036c41353c474aa86e0e7d
Tim <t.vanhijum@student.alfa-college.nl>
2023-01-09 12:17:24 +0100

    TestingScene added.
    Test Scene kopie van gameScene.
    Maak gebruik hiervan om het meeste te testen.
    
    Scene transitioning wordt anders gemaakt.

--- GokkenApp/Assets/Scripts/Game/ReturnToMainMenu.cs
+++ GokkenApp/Assets/Scripts/Game/ReturnToMainMenu.cs
@@ -5,8 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class ReturnToMainMenu : MonoBehaviour
 {
+    [SerializeField] private CanvasGroup canvasgroup;
+    private bool _fadeoutToQuit = false;
+
     public void ReturnToMenu()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+
+        if (_fadeoutToQuit)
+        {
+            if (canvasgroup.alpha >= 0)
+            {
+                canvasgroup.alpha -= Time.deltaTime;
+                if (canvasgroup.alpha == 0)
+                {
+                    _fadeoutToQuit = false;
+                }
+            }
+        }
     }
 }
