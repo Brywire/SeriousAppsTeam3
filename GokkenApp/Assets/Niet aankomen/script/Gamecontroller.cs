b3bc172e40f0d7f2abed2c8458b399eaf192c319
merlijntijssen <merlijntijssen@gmail.com>
2023-01-16 14:39:52 +0100

    nieuwe scenes

---
+++ GokkenApp/Assets/Niet aankomen/script/Gamecontroller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gamecontroller : MonoBehaviour
+{
+
+    //Load next scene
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Load previous scene
+    public void LoadPreviousScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene(9);
+    }
+}
